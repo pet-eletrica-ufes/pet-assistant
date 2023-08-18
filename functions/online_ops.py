b81c384201b8fc824aca126d0f21fb9e34408927
Antônio Sant'Ana de Oliveira <116594916+AntonioSTO@users.noreply.github.com>
2023-08-18 15:00:39 -0300

    Update online_ops.py

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -93,6 +93,7 @@ def get_random_advice(): #mesma ideia da função de piadas
 # https://subscription.packtpub.com/book/data/9781787125193/9/ch09lvl1sec61/identifying-speakers-with-voice-recognition
 # Faraday, se apresente - 'Olá, eu sou a Faraday, a assistente virtual do PET...'
 # LED responsivo 
+# Escrever em um txt as coisas que pedirem para ela. Ex "faraday anota isso" "ok estou esctando" "bla bla bla"
 
 # Adicionar piadas
 # Só acionar a leitura stt quando chamada 'Faraday'
