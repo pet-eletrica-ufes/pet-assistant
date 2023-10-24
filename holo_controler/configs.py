0f30f1d1a7d37453e94aededacab1ae518389c8e
AntonioSTO <antoniostoliveira20@gmail.com>
2023-10-24 20:03:53 -0300

    ♻️ | oracle updated to serial

--- holo_controler/configs.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class Configs:
-    cor = (255,255,255)
-    window_width, window_height = 400, 400
-    assistant_img = pygame.image.load('caminho/para/sua/imagem.png').convert_alpha()
